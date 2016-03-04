cd88cf4e4017d59ab9eb77b805919297f12c47c3
devinhuizingh <devinhuizingh@gmail.com>
2016-03-04 07:50:19 -0700

    initial commit

---
+++ myApp.js
@@ -0,0 +1,19 @@
+angular.module('myApp', []).
+	directive('makeEditable', function($sce) {
+		return {
+			restrict: 'E',
+			templateUrl: 'template.html',
+			scope: true,
+			transclude: true,
+			link: function(scope, element, attrs) {
+				//alert("yep")
+				
+				scope.click= {
+					makeEditable: function() {
+						//alert("working fine");
+						attrs.$set("contenteditable", true);
+					}
+				}
+			}
+		}
+	})
